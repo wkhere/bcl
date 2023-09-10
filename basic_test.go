a36c511df79fa33c6294235e6d5708718f9c62ee
Wojciech Kaczmarek <wojtekk@kofeina.net>
2023-09-10 18:13:12 +0200

    embed conf file for basic test

--- basic_test.go
+++ basic_test.go
@@ -1,43 +1,16 @@
 package bcl
 
-import "testing"
+import (
+	"testing"
 
-const basicInput = `
-var syncthing_port = 8384
-var local_start    = 9384
-var domain = "foo.org"
-var z0 = -z1 + +0      # forward decl
-var z1 = local_start - syncthing_port
-var z2 = -1 - (---10/2-1) + z1
-var s  = "sth" + 1 + "-" + domain
-var cond  = true
-var cond2 = false
-var cond_big = not not not not cond
+	_ "embed"
+)
 
-
-tunnel "hosty-syncthing" {
-	local_port  = local_start
-	remote_port = syncthing_port
-	host = "hosty." + domain
-	enabled = not cond2
-}
-
-tunnel "another-syncthing" {
-	host = "yet" + "." + "another.com"
-	local_port  = local_start + 1
-	remote_port = syncthing_port
-	enabled  = cond
-	prepared = true
-	started  = cond_big
-	u = 4+3*2
-	v = z0
-	x = z2 - z1
-	y = 3 - ---8/4 - (+1-3/2)
-}
-`
+//go:embed test.conf
+var basicInput []byte
 
 func basicRun() ([]Block, error) {
-	return Interp([]byte(basicInput))
+	return Interp(basicInput)
 }
 
 func TestBasic(t *testing.T) {
