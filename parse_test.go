6f66882b356a4c62884f53fcae6853f9cf5ccd39
Wojciech Kaczmarek <wojtekk@kofeina.net>
2023-09-13 17:53:33 +0200

    parse unit tests

---
+++ parse_test.go
@@ -0,0 +1,60 @@
+package bcl
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type parseTC struct {
+	input string
+	top   nTop
+	errs  string
+}
+type vmap = map[ident]expr
+
+func pvalid(inp string, top nTop) parseTC { return parseTC{inp, top, ""} }
+func perror(inp string, e string) parseTC { return parseTC{inp, nTop{}, e} }
+
+var parseTab = []parseTC{
+	pvalid(``, nTop{vars: vmap{}}),
+	perror(`!`, `line 1: syntax error: unknown char`),
+	perror(`foo `, `line 1: syntax error near ""`),
+	// ^^unfortunately error doesn't show prev token
+	perror(`foo bar`, `line 1: syntax error near "bar"`),
+
+	pvalid(`var a = 1`, nTop{vars: vmap{"a": nIntLit(1)}}),
+	pvalid(`var a = 1 + 2`, nTop{vars: vmap{
+		"a": nBinOp{"+", nIntLit(1), nIntLit(2)},
+	}}),
+
+	perror(`+ 1`, `line 1: syntax error near "+"`),
+	perror(`a + 1`, `line 1: syntax error near "+"`),
+	perror(`var a + 1`, `line 1: syntax error near "+"`),
+}
+
+func TestParse(t *testing.T) {
+	for i, tc := range parseTab {
+		top, err := parse([]byte(tc.input))
+
+		switch {
+		case err != nil && tc.errs == "":
+			t.Errorf("tc#%d unexpected error: %v", i, err)
+
+		case err != nil && tc.errs != "":
+			if !strings.Contains(err.Error(), tc.errs) {
+				t.Errorf("tc#%d error mismatch\nhave %v\nwant pattern %q",
+					i, err, tc.errs,
+				)
+			}
+
+		case err == nil && tc.errs != "":
+			t.Errorf("tc#%d have no error, want error pattern %q", i, tc.errs)
+
+		default:
+			if !reflect.DeepEqual(top, tc.top) {
+				t.Errorf("tc#%d mismatch:\nhave %+v\nwant %+v", i, top, tc.top)
+			}
+		}
+	}
+}
