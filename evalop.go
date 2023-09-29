7528f5d11a5f4d99ad4b2796bd1c449609fd91e6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2023-09-29 13:31:43 +0200

    unify eval of =, != using generics

--- evalop.go
+++ evalop.go
@@ -23,71 +23,66 @@ func evalNot(x any, typeErr func(any) error) (any, error) {
 	return nil, typeErr(x)
 }
 
-func evalEQ(l, r any, typeErr func(any, any) error) (any, error) {
+func cEQ[T comparable](l, r T) bool { return l == r }
+func cNE[T comparable](l, r T) bool { return l != r }
+
+type cFuncs struct {
+	opInt    func(int, int) bool
+	opFloat  func(float64, float64) bool
+	opString func(string, string) bool
+	opBool   func(bool, bool) bool
+}
+
+func evalCompare(ff cFuncs, l, r any, typeErr func(any, any) error) (any,
+	error) {
+
 	switch lv := l.(type) {
 	case int:
 		switch rv := r.(type) {
 		case int:
-			return lv == rv, nil
+			return ff.opInt(lv, rv), nil
 		case float64:
-			return float64(lv) == rv, nil
+			return ff.opFloat(float64(lv), rv), nil
 		}
 
 	case float64:
 		switch rv := r.(type) {
 		case float64:
-			return lv == rv, nil
+			return ff.opFloat(lv, rv), nil
 		case int:
-			return lv == float64(rv), nil
+			return ff.opFloat(lv, float64(rv)), nil
 		}
 
 	case bool:
 		switch rv := r.(type) {
 		case bool:
-			return lv == rv, nil
+			return ff.opBool(lv, rv), nil
 		}
 
 	case string:
 		switch rv := r.(type) {
 		case string:
-			return lv == rv, nil
+			return ff.opString(lv, rv), nil
 		}
 	}
+
 	return nil, typeErr(l, r)
 }
 
+func evalEQ(l, r any, typeErr func(any, any) error) (any, error) {
+	return evalCompare(
+		cFuncs{cEQ[int], cEQ[float64], cEQ[string], cEQ[bool]},
+		l, r,
+		typeErr,
+	)
+}
+
 func evalNE(l, r any, typeErr func(any, any) error) (any, error) {
-	switch lv := l.(type) {
-	case int:
-		switch rv := r.(type) {
-		case int:
-			return lv != rv, nil
-		case float64:
-			return float64(lv) != rv, nil
-		}
-
-	case float64:
-		switch rv := r.(type) {
-		case float64:
-			return lv != rv, nil
-		case int:
-			return lv != float64(rv), nil
-		}
-
-	case bool:
-		switch rv := r.(type) {
-		case bool:
-			return lv != rv, nil
-		}
-
-	case string:
-		switch rv := r.(type) {
-		case string:
-			return lv != rv, nil
-		}
-	}
-
-	return nil, typeErr(l, r)
+	return evalCompare(
+		cFuncs{cNE[int], cNE[float64], cNE[string], cNE[bool]},
+		l, r,
+		typeErr,
+	)
 }
 
 func evalPlus(l, r any, typeErr func(any, any) error) (any, error) {
