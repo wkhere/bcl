e09842da053e886d8cb3daf139ea91af7ad546b0
Wojciech Kaczmarek <wojtekk@kofeina.net>
2024-04-24 14:17:02 +0200

    API outline in overview doc

--- doc.go
+++ doc.go
@@ -1,3 +1,8 @@
 // Package bcl provides interpretation of the Basic Configuration Language
 // and storing the evaluated result in dynamic Blocks or static structs.
+//
+//   - [Interpret] parses then executes definitions from a BCL file, then creates
+//     Blocks
+//   - [CopyBlocks] takes Blocks and saves the content in static Go structs
+//   - [Unmarshal] = [Interpret] + [CopyBlocks]
 package bcl
