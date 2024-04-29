17b7562a7fbf1ce33bfdc18fb1c6b0932a62df9d
Wojciech Kaczmarek <wojtekk@kofeina.net>
2024-04-30 00:54:12 +0200

    double-revert "encoding: big-endian u16"
    OK, so here's what I got:
    
    Sqlite4's uvarint aka mohae/uvarint is the best for my purpose:
    it can encode indexes <= 240 with just 1B. Still, it is big-endian,
    so I am changing also u16 encoding used to big-endian, for consistency.
    
    This reverts commit 98f13d654fd470284b76c39faa579440bdaa6b52.

--- encoding.go
+++ encoding.go
@@ -3,12 +3,12 @@ package bcl
 import "github.com/mohae/uvarint"
 
 func u16ToBytes(p []byte, x uint16) {
-	p[0] = byte(x & 0xff)
-	p[1] = byte(x >> 8 & 0xff)
+	p[0] = byte(x >> 8 & 0xff)
+	p[1] = byte(x & 0xff)
 }
 
 func u16FromBytes(b []byte) uint16 {
-	return uint16(b[1])<<8 | uint16(b[0])
+	return uint16(b[0])<<8 | uint16(b[1])
 }
 
 func uvarintToBytes(p []byte, x uint64) int {
