6f6826b32482d95c6e2e5de164e64e39e4d87e8b
Wojciech Kaczmarek <wojtekk@kofeina.net>
2025-02-26 22:28:46 +0100

    move snake stuff back to reflect.go where it belongs

--- reflect.go
+++ reflect.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 func copyBlocks(target any, binding Binding) error {
@@ -130,3 +132,35 @@ type fieldMappingErr string
 func (e fieldMappingErr) Error() string { return string(e) }
 
 var blockType = reflect.TypeOf(Block{})
+
+func unsnakeMatcher(snake string) func(string) bool {
+	u := strings.ReplaceAll(snake, "_", "")
+	return func(s string) bool {
+		return strings.EqualFold(s, u)
+	}
+}
+
+func unsnakeEq(orig, snake string) bool {
+	return unsnakeMatcher(snake)(orig)
+}
+
+func snake(input string) string {
+	nextRune := func(idx int) rune { r, _ := utf8.DecodeRuneInString(input[idx:]); return r }
+
+	var b strings.Builder
+	var prev rune
+
+	for i, v := range input {
+		if unicode.IsUpper(v) {
+			if i > 0 && (unicode.IsLower(prev) ||
+				unicode.IsLower(nextRune(i+utf8.RuneLen(v)))) {
+				b.WriteByte('_')
+			}
+			b.WriteRune(unicode.ToLower(v))
+		} else {
+			b.WriteRune(v)
+		}
+		prev = v
+	}
+	return b.String()
+}
--- reflect_test.go
+++ reflect_test.go
@@ -264,3 +264,50 @@ func TestReflect(t *testing.T) {
 		}
 	}
 }
+
+var snakeTab = []struct{ input, want string }{
+	{"", ""},
+	{"x", "x"},
+	{"X", "x"},
+	{"foo", "foo"},
+	{"Foo", "foo"},
+
+	{"fooBar", "foo_bar"},
+	{"FooBar", "foo_bar"},
+	{"FooBAR", "foo_bar"},
+	{"FOOBar", "foo_bar"},
+	{"FOoBar", "f_oo_bar"},
+
+	{"FoOBar", "fo_o_bar"},
+	{"ab", "ab"},
+	{"aB", "a_b"},
+	{"Ab", "ab"},
+	{"AB", "ab"},
+
+	{"SNAKESEverywhere", "snakes_everywhere"},
+	{"parseURLAndStuff", "parse_url_and_stuff"},
+	{"ąćęŁŃÓŚŹŻ", "ąćę_łńóśźż"},
+	{"ĄĆęŁŃÓŚŹŻ", "ą_ćę_łńóśźż"},
+	{"ĄĆĘŁŃóśźż", "ąćęł_ńóśźż"},
+}
+
+func TestSnakeUnsnake(t *testing.T) {
+
+	for i, tc := range snakeTab {
+		s := snake(tc.input)
+		if s != tc.want {
+			t.Errorf("tc#%d mismatch: have %q, want %q", i, s, tc.want)
+		}
+		if !unsnakeEq(tc.input, s) {
+			t.Errorf("tc#%d unsnakeEq failed", i)
+		}
+	}
+}
+
+func BenchmarkSnake(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range snakeTab[:20] {
+			_ = snake(tc.input)
+		}
+	}
+}
